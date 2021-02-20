65f688d98ed939435817a3e6ddd7c382721e4ebe
SebastienLatouche <sebastien.latouche.28@gmail.com>
2021-02-20 17:20:48 +0100

    header & title & to forum button ok in integration

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,13 +4,11 @@ import './app.scss';
 
 import Header from 'src/components/Header';
 import Footer from 'src/components/Footer';
-import Nav from 'src/components/Nav';
 import Page from 'src/components/Page';
 
 const App = () => (
   <div className="app">
     <Header />
-    <Nav />
     <Page />
     <Footer />
   </div>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,11 @@ import './header.scss';
 
 const Header = () => (
   <header className="header">
-    <h1 className="header_title">Header</h1>
+    <h1 className="header_title">auto salon france</h1>
+    <p className="header_content">
+      <span className="header_content-presentation">Photothèque de véhicules prises en salon, rasso, circuit...</span>
+      <span className="header_content-toForum">Forum &gt;</span>
+    </p>
   </header>
 );
 
